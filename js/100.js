7e58947b6ae2fe926baf08ea8ea36eb650ea3644
NeverNG <wanghong_hui@live.com>
2020-12-08 09:35:34 +0800

    45min js 练习

---
+++ js/100.js
@@ -0,0 +1,31 @@
+console.log('helloworld');
+console.error('error');
+console.warn('this is warning');
+//var
+//let,const
+let age = 30;
+age = 31;
+console.log(age);
+
+let  score;
+score = 10;
+console.log(score);
+
+const year = 2020;
+//常量不能改变赋值,生命const变量时必须赋值;
+console.log(year);
+//String,Numbers,Boolean,null,undefined,--symbol
+const name = 'John';
+const age1 = 30;
+const rating = 4.5;
+const isCool = true;
+const x = null;
+const y =undefined;
+let z;
+console.log(typeof name);
+console.log(typeof age1);
+console.log(typeof rating);
+console.log(typeof isCool);
+console.log(typeof x);
+console.log(typeof y);
+console.log(typeof z);
---
+++ js/45.js
@@ -0,0 +1,34 @@
+console.log("hello world");
+
+let name = 'masj';
+console.log(name);
+//Connot be a reserved keyword
+//Should be meaningful
+//Cannot start with a number (1name)
+//Cannot contain a space or htphen
+//Are case-sensitive
+
+
+
+let interestRate = 0.3;
+interestRate = 1;
+console.log(interestRate);
+
+const insterestRate1 = 0.5;
+//const 定义常量，需要赋初始值，不能重新赋值
+// insterestRate1 = 5;
+ console.log(insterestRate1);
+
+ //基础类型
+let firstName = 'Mosh';//String Literal
+let age = 30;//Number Literal
+let isApproved = true; //Boolean Literal
+let lastName = undefined;// undefined 既是变量类型，也是变量值
+let selectedColor = null;//清空一个变量值用null null是object对象
+
+//JavaScript 动态语言 变量类型可以改变
+
+//引用类型
+
+
+
